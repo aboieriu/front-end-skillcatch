4fd8a734db32aa171dd7c234c1d9fcbda9977de4
CatalinVlad <cata_bv94@yahoo.com>
2015-10-19 12:35:08 +0300

    authorization structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 define(function(require, exports, module) {
     var app = require('app');
     var appConfig = require('modules/app/main');
+    var setup = require('modules/app/initializers/setup');
 
     app.on("initialize:after", function(options){
         Backbone.history.start();
     });
+
+
 
     app.start();
 }
