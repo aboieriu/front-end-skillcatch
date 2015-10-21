983af7f792d761c0a87675ab835ff790ace97e7d
CatalinVlad <cata_bv94@yahoo.com>
2015-10-21 12:25:11 +0300

    authorization structure

--- js/modules/app/initializers/setup.js
+++ js/modules/app/initializers/setup.js
@@ -1,13 +1,15 @@
 define(function (require, exports, module) {
     var app = require('app');
+    var Backbone = require('backbone');
 
     app.addInitializer(function () {
         $.ajaxSetup({
             complete: function (response) {
-                if (response.statusCode === 401) {
-                    alert('status 401');
-                } else if (response.statusCode === 403) {
-                    alert('status 403');
+                if (response.status === 401) {
+
+                    Backbone.history.navigate('/test/401',true);
+                } else if (response.status === 403) {
+                    Backbone.history.navigate('/test/403',true);
                 }
                 alert("succes");
 
