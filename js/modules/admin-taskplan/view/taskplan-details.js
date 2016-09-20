9a5c88c311bb1eb40f61f4530eec01f7800ec4f8
Cringanu Ionut <cringanu_ionutz@yahoo.ro>
2016-09-20 11:18:13 +0300

    Admin side components

--- js/modules/admin-taskplan/view/taskplan-details.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-
-    var Backbone = require('backbone');
-    var Marionette = require('marionette');
-
-    module.exports = Marionette.ItemView.extend({
-        template: '#admin-taskplan-taskplan-details-view'
-    });
-});
--- js/modules/task/model/task.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-    var Backbone = require('backbone');
-
-    module.exports = Backbone.Model.extend({
-        url :function() {
-            return window.baseApiPath +'/api/loggedUser/tasks/'+this.get('id');
-        }
-    });
-});
