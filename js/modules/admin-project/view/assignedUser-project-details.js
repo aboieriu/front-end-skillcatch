0d604cb2c219a3e4b17c78cede8c545441445208
Cringanu Ionut <cringanu_ionutz@yahoo.ro>
2016-09-20 12:06:35 +0300

    Admin side components

--- js/modules/admin-project/view/assignedUser-project-details.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-
-    var Backbone = require('backbone');
-    var Marionette = require('marionette');
-
-    module.exports = Marionette.ItemView.extend({
-        template: '#admin-project-assignedUser-project-details-view'
-    });
-});
--- js/modules/badge/view/user-task-badges-collection-view.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-    var Marionette = require('marionette');
-    var UserBadgeItemView = require('./user-task-badges-item-view');
-
-    module.exports = Marionette.CollectionView.extend({
-        tagName:'ul'
-        , itemView: UserBadgeItemView
-    });
-});
